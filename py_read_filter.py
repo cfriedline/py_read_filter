2471bffa01c45b16b97ff8609c3595ff175d4235
Chris Friedline <cfriedline@vcu.edu>
2014-05-30 01:18:21 -0400

    drop log.warn

--- py_read_filter.py
+++ py_read_filter.py
@@ -283,7 +283,6 @@ def process_paired(args):
         pairs = 0
 
     for temp1, temp2 in izip(tmpfiles[0], tmpfiles[1]):
-        log.warn(temp1, temp2)
         p = process_paired_files(temp1, temp2, args)
         pairs += 1
         results.append(p)
